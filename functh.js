c47a950b644e9f62c2d116f12fcfd3e5509c747f
Александра Бороданева <balevna@mail.ru>
2022-12-25 23:18:01 +0300

    Java

---
+++ functh.js
@@ -0,0 +1,53 @@
+function summary() {
+    document.getElementById("pop").classList.toggle("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.btn')) {
+  
+      let dropdowns = document.getElementsByClassName("menu");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+function shrek(){
+    window.open("file:///C:/Users/aleks/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/%D0%A3%D1%87%D0%B5%D0%B1%D0%B0/3%20%D0%BA%D1%83%D1%80%D1%81/1%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/WEB/WEB_RGR/shrek.html")
+}
+function mad(){
+    window.open("file:///C:/Users/aleks/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/%D0%A3%D1%87%D0%B5%D0%B1%D0%B0/3%20%D0%BA%D1%83%D1%80%D1%81/1%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/WEB/WEB_RGR/mad.html")
+}
+function led(){
+    window.open("file:///C:/Users/aleks/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/%D0%A3%D1%87%D0%B5%D0%B1%D0%B0/3%20%D0%BA%D1%83%D1%80%D1%81/1%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/WEB/WEB_RGR/led.html")
+}
+function simps(){
+    window.open("file:///C:/Users/aleks/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/%D0%A3%D1%87%D0%B5%D0%B1%D0%B0/3%20%D0%BA%D1%83%D1%80%D1%81/1%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/WEB/WEB_RGR/simps.html")
+}
+function corp(){
+    window.open("file:///C:/Users/aleks/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/%D0%A3%D1%87%D0%B5%D0%B1%D0%B0/3%20%D0%BA%D1%83%D1%80%D1%81/1%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/WEB/WEB_RGR/corp.html")
+}
+function html(){
+    window.open("file:///C:/Users/aleks/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/%D0%A3%D1%87%D0%B5%D0%B1%D0%B0/3%20%D0%BA%D1%83%D1%80%D1%81/1%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/WEB/WEB_RGR/code.html")
+};
+function css(){
+    window.open("file:///C:/Users/aleks/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/%D0%A3%D1%87%D0%B5%D0%B1%D0%B0/3%20%D0%BA%D1%83%D1%80%D1%81/1%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/WEB/WEB_RGR/codecss.html")
+}
+function calc(){
+    window.open("file:///C:/Users/aleks/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/%D0%A3%D1%87%D0%B5%D0%B1%D0%B0/3%20%D0%BA%D1%83%D1%80%D1%81/1%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/WEB/WEB_RGR/calc.html")
+}
+
+
+ function myFunction(elmnt, clr) {
+  elmnt.style.color = clr;
+}
+
+
+function setclick(color){
+    document.body.style.background = color
+}
+
